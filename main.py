395af9e1da0cc3a97f546044c2c9589e59ae492a
danny50167 <danny.rollingsky@gmail.com>
2023-07-09 23:01:28 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+import pygame
+import random
+
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 60
+
+# Define Colors 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+## initialize pygame and create window
+pygame.init()
+pygame.mixer.init()  ## For sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("게임 이름")
+clock = pygame.time.Clock()     ## For syncing the FPS
+
+
+## group all the sprites together for ease of update
+all_sprites = pygame.sprite.group()
+
+## Game loop
+running = True
+while running:
+
+    #1 Process input/events
+    clock.tick(FPS)     ## will make the loop run at the same speed all the time
+    for event in pygame.event.get():        # gets all the events which have occured till now and keeps tab of them.
+        ## listening for the the X button at the top
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    #2 Update
+    all_sprites.update()
+
+
+    #3 Draw/render
+    screen.fill(RED)
+
+    
+
+    all_sprites.draw(screen)
+    ########################
+
+    ### Your code comes here
+
+    ########################
+
+    ## Done after drawing everything to the screen
+    pygame.display.flip()       
+
+pygame.quit()
